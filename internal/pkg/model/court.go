594bcd6bda1dce858e10477f446e362f19ecd183
cianoyang <cianoyang@tencent.com>
2022-12-07 19:04:31 +0800

    add files

--- internal/pkg/model/court.go
+++ internal/pkg/model/court.go
@@ -1,1 +1,46 @@
 package model
+
+import "wxcloudrun-golang/internal/pkg/db"
+
+type Court struct {
+	ID       int32
+	Name     string
+	Location string
+	Info     string
+}
+
+// TableName get sql table name.获取数据库名字
+func (obj *Court) TableName() string {
+	return "t_court"
+}
+
+// Create 创建记录
+func (obj *Court) Create(count *Court) (*Court, error) {
+	err := db.Get().Create(count).Error
+	return count, err
+}
+
+// Get 获取
+func (obj *Court) Get(count *Court) (*Court, error) {
+	result := new(Court)
+	err := db.Get().Table(obj.TableName()).Where(count).First(result).Error
+	return result, err
+}
+
+// Gets 获取批量结果
+func (obj *Court) Gets(count *Court) ([]Court, error) {
+	results := make([]Court, 0)
+	err := db.Get().Table(obj.TableName()).Where(count).Find(&results).Error
+	return results, err
+}
+
+// Update 更新
+func (obj *Court) Update(count *Court) (*Court, error) {
+	err := db.Get().Table(obj.TableName()).Where("id = ?", count.ID).Updates(count).Error
+	return count, err
+}
+
+// Delete 删除
+func (obj *Court) Delete(count *Court) error {
+	return db.Get().Delete(count, "id = ?", count.ID).Error
+}
--- internal/pkg/model/event.go
+++ internal/pkg/model/event.go
@@ -1,1 +1,52 @@
 package model
+
+import (
+	"time"
+	"wxcloudrun-golang/internal/pkg/db"
+)
+
+type Event struct {
+	ID          int32
+	UserOpenID  int32
+	CourtID     int32
+	StartTime   time.Time
+	EndTime     time.Time
+	CreatedTime time.Time
+	UpdatedTime time.Time
+}
+
+// TableName get sql table name.获取数据库名字
+func (obj *Event) TableName() string {
+	return "t_event"
+}
+
+// Create 创建记录
+func (obj *Event) Create(event *Event) (*Event, error) {
+	err := db.Get().Create(event).Error
+	return event, err
+}
+
+// Get 获取
+func (obj *Event) Get(event *Event) (*Event, error) {
+	result := new(Event)
+	err := db.Get().Table(obj.TableName()).Where(event).First(result).Error
+	return result, err
+}
+
+// Gets 获取批量结果
+func (obj *Event) Gets(event *Event) ([]Event, error) {
+	results := make([]Event, 0)
+	err := db.Get().Table(obj.TableName()).Where(event).Find(&results).Error
+	return results, err
+}
+
+// Update 更新
+func (obj *Event) Update(event *Event) (*Event, error) {
+	err := db.Get().Table(obj.TableName()).Where("id = ?", event.ID).Updates(event).Error
+	return event, err
+}
+
+// Delete 删除
+func (obj *Event) Delete(event *Event) error {
+	return db.Get().Delete(event, "id = ?", event.ID).Error
+}
